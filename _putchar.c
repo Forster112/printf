cf6e02035aaf6968f3fd2965262e627e0e143e13
Forster112 <forstermichael112@gmail.com>
2022-04-21 20:53:38 +0100

    fixing betty error on putchar

--- _putchar.c
+++ _putchar.c
@@ -2,13 +2,11 @@
 
 /**
  * _putchar - prints out a single character to the stdout
- *
  * @c: the char to be printed out
  *
  *Return: returns an integer
  */
-
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+		return (write(1, &c, 1));
 }
