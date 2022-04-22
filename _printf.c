34b68f45d94459911e2f3c1fadb1545aaf856fad
Forster112 <forstermichael112@gmail.com>
2022-04-21 17:51:57 -0700

    trying

--- _printf.c
+++ _printf.c
@@ -18,50 +18,40 @@ int _printf(const char *format, ...)
 		int (*m)(va_list);
 		va_list args;
 
-        va_start(args, format);
-        
-        if (format[0] == '%' && format[1] == '\0')
+		va_start(args, format);
+		if (format[0] == '%' && format[1] == '\0')
+	{
+		return (-1);
+	}
+	
+	while (format[i] != '\0')
         {
-
-            return (-1);
-        }
-
-        while (format[i] != '\0')
-        {
-            if (format[i] == '%')
-            {
-                if (format[i + 1] == '%')
-                {
-                    count += _putchar(format[i]);
-                    i += 2;
-                }
-                else if (format[i + 1] == '\0')
-                {
-                    count += _putchar(format[i]);
-                    break;
-                }
-                else
-                {
-                    m = spec_checker(format[i + 1]);
-                    if (m)
-                    {
-                        count = m(args);
-                    }
-                    else
-                    {
-                        count = _putchar(format[i]) + _putchar(format[i + 1]);
-                    }
-                    i += 2;
-                }
-            }
-            else
-            {
-                count += _putchar(format[i]);
-                i++;
-            }
-        }
-        va_end(args);
-        return (count);
-    }
-    return (-1);
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '%')
+			{
+				count += _putchar(format[i]);
+				i += 2;
+			} else if (format[i + 1] == '\0')
+			{
+				count += _putchar(format[i]);
+			} else
+			{
+				m = spec_checker(format[i + 1]);
+				if (m)
+				{
+					count = m(args);
+				} else
+				{
+					count = _putchar(format[i]) + _putchar(format[i + 1]);
+				} i += 2;
+			}
+		} else
+		{
+			count += _putchar(format[i]);
+			i++;
+		}
+	} va_end(args);
+	return (count);
+	} return (-1);
 }
