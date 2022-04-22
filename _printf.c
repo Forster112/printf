37703992751fe6aa647b323d87df0a797a880c81
Forster112 <forstermichael112@gmail.com>
2022-04-22 09:14:21 -0700

    working on betty

--- _printf.c
+++ _printf.c
@@ -20,38 +20,37 @@ int _printf(const char *format, ...)
 
 		va_start(args, format);
 		if (format[0] == '%' && format[1] == '\0')
-	{
-		return (-1);
-	}
-	
-	while (format[i] != '\0')
-        {
-		if (format[i] == '%')
 		{
-			if (format[i + 1] == '%')
+			return (-1);
+		}
+		while (format[i] != '\0')
+		{
+			if (format[i] == '%')
 			{
-				count += _putchar(format[i]);
-				i += 2;
-			} else if (format[i + 1] == '\0')
-			{
-				count += _putchar(format[i]);
-			} else
-			{
-				m = spec_checker(format[i + 1]);
-				if (m)
+				if (format[i + 1] == '%')
 				{
-					count = m(args);
+					count += _putchar(format[i]);
+					i += 2;
+				} else if (format[i + 1] == '\0')
+				{
+					count += _putchar(format[i]);
 				} else
 				{
-					count = _putchar(format[i]) + _putchar(format[i + 1]);
-				} i += 2;
-			}
-		} else
-		{
-			count += _putchar(format[i]);
-			i++;
+					m = spec_checker(format[i + 1]);
+					if (m)
+					{
+						count = m(args);
+					} else
+				}
+				count = _putchar(format[i]) + _putchar(format[i + 1]);
+			} i += 2;
 		}
-	} va_end(args);
-	return (count);
-	} return (-1);
+	} else
+	{
+		count += _putchar(format[i]);
+		i++;
+	}
+} va_end(args);
+return (count);
+} return (-1);
 }
